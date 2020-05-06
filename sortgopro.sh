b13d242c59ccde0fe35d2defe06b175745a438d7
Peter Keel <git@discordia.ch>
2020-05-07 00:24:23 +0200

    sortgopro.sh: shellcheck'ed

--- sortgopro.sh
+++ sortgopro.sh
@@ -8,11 +8,11 @@
 # Version:  0.1
 # License:  Public Domain
 # 
-rm *.THM 2> /dev/null
-mmv '*.JPG' '#1.jpg' 2>&1 > /dev/null
-mmv '*.MP4' '#1.mp4' 2>&1 > /dev/null
-mmv 'GOPR*.mp4' 'GO-#1-00.mp4' 2>&1 > /dev/null
-mmv 'GP??*.mp4' 'GO-#3-#1#2.mp4' 2>&1 > /dev/null
-mmv 'GOPR*.mkv' 'GO-#1-00.mkv' 2>&1 > /dev/null
-mmv 'GP??*.mkv' 'GO-#3-#1#2.mkv' 2>&1 > /dev/null
+rm ./*.THM 2> /dev/null
+mmv '*.JPG' '#1.jpg'> /dev/null 2>&1
+mmv '*.MP4' '#1.mp4'> /dev/null 2>&1
+mmv 'GOPR*.mp4' 'GO-#1-00.mp4'> /dev/null 2>&1
+mmv 'GP??*.mp4' 'GO-#3-#1#2.mp4'> /dev/null 2>&1
+mmv 'GOPR*.mkv' 'GO-#1-00.mkv'> /dev/null 2>&1
+mmv 'GP??*.mkv' 'GO-#3-#1#2.mkv'> /dev/null 2>&1
 
